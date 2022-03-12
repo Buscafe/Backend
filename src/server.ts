b79a2bdd9824e0879705918a49491258e60a88d0
Igor <igorcbraz1@gmail.com>
2022-03-12 14:11:13 -0300

    Deploy Heroku

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import express from 'express'
 import { router } from './routes';
 
 const app  = express();
-const port = 3333;
+const port = process.env.PORT || 3333;
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', "*");
