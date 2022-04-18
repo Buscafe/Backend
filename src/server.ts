feaadf7404af9d6a6f90ddd1e8798d1c62ec1187
LuisFernandoPBPereira <luisfernandopbp@gmail.com>
2022-04-18 20:57:47 -0300

    adding socket.io logic

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,14 @@
 import express from 'express'
 import { router } from './routes';
+import { Server } from "socket.io";
 
 const app  = express();
 const port = process.env.PORT || 3333;
+
+const io = new Server(3001)
+let messages = []
+
+
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', "*");
@@ -16,3 +22,14 @@ app.use(router);
 
 
 app.listen(port, () => { console.log(`🚀 Sever is running on port ${port}`) });
+
+io.on("connection", (socket) => {
+    console.log('socket-backend foi conectado')   
+    socket.on('sendMessage', data =>{
+            messages.push(data);
+            console.log(data);
+            socket.broadcast.emit('receivedMessage', data);
+    })
+});
+
+
