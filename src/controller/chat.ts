137957e99d8df21005034c53aac5d749af6164eb
Gabriel-limadev <gabrielvitor180701@gmail.com>
2022-04-24 16:18:40 -0300

    Mudando grupos de conversas de acordo com a igreja filiada

--- src/controller/chat.ts
+++ src/controller/chat.ts
@@ -1,11 +1,26 @@
 import { validateFields } from "../utils/validateHasProperty";
 import { Request, Response } from "express";
 
-import { findRooms } from '../models/M_chat'
+import { findChurches, findRooms } from '../models/M_chat'
 
 
 export class ChatController {
     async getRooms(req: Request, res: Response){
+        const responseValidate = validateFields(req.params, 'id_user', 'church_name');
+
+        responseValidate.map(validate => {
+            if(!validate.exists){
+                return res.json({'Error': `Missing parameter ${validate.field}`});
+            } else if (validate.empty){
+                return res.json({'Error': `Parameter ${validate.field} are empty`});
+            } 
+        });
+        
+        const getRoomsResponse = await findRooms(Number(req.params.id_user), String(req.params.church_name));
+
+        return res.json(getRoomsResponse);
+    }
+    async getChurches(req: Request, res: Response){
         const responseValidate = validateFields(req.params, 'id_user');
 
         responseValidate.map(validate => {
@@ -16,7 +31,7 @@ export class ChatController {
             } 
         });
         
-        const getRoomsResponse = await findRooms(Number(req.params.id_user));
+        const getRoomsResponse = await findChurches(Number(req.params.id_user));
 
         return res.json(getRoomsResponse);
     }
--- src/models/M_chat.ts
+++ src/models/M_chat.ts
@@ -1,8 +1,28 @@
 import { rooms } from '../services/room' 
 
-export async function findRooms(id_user: number){
+export async function findRooms(id_user: number, church_name: String){
     try {
         const allRooms = await rooms.find({
+            name: church_name,
+            chats: {
+                $elemMatch: {
+                    users: {
+                        $elemMatch: { id_user }
+                    }
+                }
+            }
+        });
+        return allRooms
+    } catch (error) {
+        return {
+            'status' : 'error',
+            'err' : error
+        } 
+    }
+}
+export async function findChurches(id_user: number){
+    try {
+        const allChurches = await rooms.find({
             chats: {
                 $elemMatch: {
                     users: {
@@ -12,7 +32,7 @@ export async function findRooms(id_user: number){
             }
         });
 
-        return allRooms
+        return allChurches
     } catch (error) {
         return {
             'status' : 'error',
